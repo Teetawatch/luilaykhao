48f6787c5bcded4f488916e2e0db7cd4bc2d9c16
Teetawatch Phiphatdetthana <teetawatch.p@gmail.com>
2026-04-18 10:44:01 +0700

    fix: resolve blank white page on production and fixed unhead context error by using createHead

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { createUnhead } from '@unhead/vue';
+import { createHead } from '@unhead/vue';
 import router from './router';
 import App from './App.vue';
 
 const app = createApp(App);
-const head = createUnhead();
+const head = createHead();
 app.use(head);
 app.use(createPinia());
 app.use(router);
