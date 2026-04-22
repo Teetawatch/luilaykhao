18ea3c8f196b2c3f96d7b98d6ae8f468f65e8b2d
Teetawatch <teetawatch.p@gmail.com>
2026-04-22 20:23:29 +0700

    feat: final tawk.to widget script update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -48,12 +48,14 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/69e8c26faed59b1c34e3582a/1jmqkui6a';
+s1.src='https://embed.tawk.to/69e8c26faed59b1c34e3582a/1jmqllhq6';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
 <!--End of Tawk.to Script-->
+
+
 </body>
 </html>
