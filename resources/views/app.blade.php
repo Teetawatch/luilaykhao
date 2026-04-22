ae0ed1fc51bc88e9d6f06b8b8bcce8f6217fd685
Teetawatch <teetawatch.p@gmail.com>
2026-04-22 20:09:48 +0700

    feat: update tawk.to widget script with new configuration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,17 +42,17 @@
 <body class="bg-[var(--color-white)] text-[var(--color-text-mid)] antialiased" style="font-family: 'Anuphan', sans-serif;">
     <div id="app"></div>
 
-    <div id='tawk_69e8c26faed59b1c34e3582a'></div>
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_69e8c26faed59b1c34e3582a';
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/69e8c26faed59b1c34e3582a/1jmqknnv7';
+s1.src='https://embed.tawk.to/69e8c26faed59b1c34e3582a/1jmqkui6a';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);})();
+s0.parentNode.insertBefore(s1,s0);
+})();
 </script>
 <!--End of Tawk.to Script-->
 </body>
