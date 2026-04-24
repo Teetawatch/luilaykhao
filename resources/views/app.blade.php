334069e29a7fd92d8711e2419b66359b89ee79f4
Teetawatch Phiphatdetthana <teetawatch.p@gmail.com>
2026-04-24 20:44:08 +0700

    Update Tawk.to chat widget script ID

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -201,7 +201,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/69e8c26faed59b1c34e3582a/1jmqllhq6';
+s1.src='https://embed.tawk.to/69e8c26faed59b1c34e3582a/1jmqjf0vt';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
